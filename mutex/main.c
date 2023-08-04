9b6b24cb529cfe4cf3da2a05c2ab217c89a632ce
Netony <kuznecov98@naver.com>
2023-08-04 11:47:02 +0900

    test

---
+++ mutex/main.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+typedef struct s_test
+{
+	pthread_t	thread;
+}	t_test;
+
+int	main(void)
+{
+	pthread_t	threads[5];
+
+
+	while 
+}
+
+void	*f(void *f_args)
+{
+	
+}
+
+int	eat(void)
+{
+	ft_setstatus(EAT);
+	action();
+	ft_setstatus(NONE);
+}
+
+int	action(void)
+{
+	int duration;
+	int	start;
+	int	time;
+
+	start = ft_gettime(mutex);
+	while (1)
+	{
+		time = ft_gettime(mutex) - start;
+		if (time >= duration)
+			break ;
+	}
+}
+
+int	take(void)
+{
+	
+}
+
+int	eat(void)
+{
+	int	eating_time;
+	int	start;
+
+	eating_time = 5;
+	whiel 
+}
