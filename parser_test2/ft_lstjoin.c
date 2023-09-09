ad0ab11a9f4263ab26972b0b653186af76e7a070
Daemyeong Jeon <dajeon@c5r3s4.42seoul.kr>
2023-09-09 21:18:48 +0900

    test

--- parser_test2/ft_lstjoin.c
+++ parser_test2/ft_lstjoin.c
@@ -1,61 +1,64 @@
-t_list	*ft_param_list(t_list *text_list)
+t_list	*ft_emptynew(void);
+t_list	*ft_parse_escape(char *s, int *i);
+
+t_list	*ft_parse_dquote(t_info *info, char *s, int *i)
 {
+	t_list	*node;
+
+	node = ft_parse_dquote_(info, s, i);
+	if (s[*i] == '\0')
+	{
+		error("bash: ");
+		return (NULL);
+	}
+	if (list == NULL)
+		return (NULL);
+		ft_txtclear(&token);
+	node = ft_dquote_merge(token);
+	ft_txtclear(&token);
+	if (node == NULL)
+	{
+		
+	}
+	*i += 1;
+}
+
+t_list	*ft_parse_dquote(t_info *info, char *s, int *i)
+{
+	t_list	*merge;
 	t_list	*list;
 	t_list	*node;
-	t_list	*cur;
-	char	*path;
 
-	cur = redi_list;
+	*i += 1;
 	list = NULL;
-	while (cur)
+	while (s[*i])
 	{
-		ft_skip_space(&cur);
-		if (cur == NULL)
+		if (s[*i] == '\"')
 			break ;
-		node = ft_paramnew(&cur);
+		node = ft_dquote_node(info, s, i);
 		if (node == NULL)
 		{
 			ft_txtclear(&list);
 			return (NULL);
 		}
 		ft_lstadd_back(&list, node);
-		cur = cur->next;
 	}
-	return (list);
+	merge = ft_dquote_merge(list);
+	ft_txtclear(&list);
+	return (merge);
 }
 
-t_list	*ft_paramnew(t_list **cur)
+t_list	*ft_dquote_node(t_info *info, char *s, int *i)
 {
-	t_list	*node;
-	char	*path;
-
-	path = ft_lstjoin(&cur);
-	if (path == NULL)
-		return (NULL);
-	node = ft_txtnew("param", path);
-	if (node == NULL)
-	{
-		free(path);
-		return (NULL);
-	}
-	return (node);
+	if (s[*i] == '$')
+		node = ft_parse_env(info, s, i);
+	else if (s[*i] == '\\')
+		node = ft_parse_escape(s, i, "$\"\\");
+	else
+		node = ft_parse_token(s, i, "$\"\\");
 }
 
-int	ft_skip_space(t_list **list)
-{
-	t_redi	*redi;
-
-	while (*list)
-	{
-		redi = (*list)->content;
-		if (ft_strcmp(redi->type, "text"))
-			break ;
-		*list = (*list)->next;
-	}
-	return (0);
-}
-
-char	*ft_lstjoin(t_list **list)
+char	*ft_dquote_lstjoin(t_list *list)
 {
 	t_redi	*redi;
 	char	*join;
@@ -64,22 +67,49 @@ char	*ft_lstjoin(t_list **list)
 	join = ft_strdup("");
 	if (join == NULL)
 		return (NULL);
-	while (*list)
+	while (list)
 	{
-		redi = (*list)->content;
-		if (ft_strcmp(redi->type, "space"))
-			break ;
+		redi = list->content;
 		temp = join;
 		join = ft_strjoin(join, redi->path);
 		free(temp);
 		if (join == NULL)
 			return (NULL);
-		*list = (*list)->next;
-	}
-	if (*join = '\0')
-	{
-		free(join);
-		join = NULL;
+		list = list->next;
 	}
 	return (join);
+}
+
+t_list	*ft_dquote_merge(t_list *list)
+{
+	t_list	*node;
+	char	*path;
+
+	path = ft_dquote_lstjoin(list);
+	if (path == NULL)
+		return (NULL);
+	node = ft_txtnew("text", path);
+	if (node == NULL)
+	{
+		free(path);
+		return (NULL);
+	}
+	return (node);
+}
+
+t_list	*ft_emptynew(void)
+{
+	char	*s;
+	t_list	*node;
+
+	s = ft_strdup("");
+	if (s == NULL)
+		return (NULL);
+	node = ft_txtnew("text", s);
+	if (node == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+	return (node);
 }
